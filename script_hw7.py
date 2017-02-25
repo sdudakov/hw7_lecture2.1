8575b2f5f5bff2c1912721cd1c4928bbf20564c0
Сергей Дудаков <grey@kmail.ru>
2017-02-25 15:12:24 +0300

    домашняя работа 7 по лекции 2.1

---
+++ script_hw7.py
@@ -0,0 +1,62 @@
+# cook_book = {
+#   'яйчница': [
+#     {'ingridient_name': 'яйца', 'quantity': 2, 'measure': 'шт.'},
+#     {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'}
+#     ],
+#   'стейк': [
+#     {'ingridient_name': 'говядина', 'quantity': 300, 'measure': 'гр.'},
+#     {'ingridient_name': 'специи', 'quantity': 5, 'measure': 'гр.'},    
+#     {'ingridient_name': 'масло', 'quantity': 10, 'measure': 'мл.'}
+#     ],
+#   'салат': [
+#     {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'},
+#     {'ingridient_name': 'огурцы', 'quantity': 100, 'measure': 'гр.'},    
+#     {'ingridient_name': 'масло', 'quantity': 100, 'measure': 'мл.'},
+#     {'ingridient_name': 'лук', 'quantity': 1, 'measure': 'шт.'}
+#     ]
+#   }
+
+shop_list = {}
+with open("data_hw7.txt", "r", encoding="utf-8") as f:
+  for dish in f:
+    ingridient_count = int(f.readline())
+        # print("dish = {}ingridient_count = {}" .format(dish, ingridient_count))
+    for _ in range(ingridient_count):
+      ingridient = f.readline()
+      ingridient = ingridient.strip()
+      ingridient = ingridient.split(" | ")
+      new_shop_list_item = dict({"ingridient_name": ingridient[0], "quantity": ingridient[1], "measure": ingridient[2]})
+      print(new_shop_list_item)
+      # shop_list[dish.strip()] = ingridient_list
+      # shop_list['ingridient_name'] = ingridient[0]
+      # shop_list['quantity'] = ingridient[1]
+      # shop_list['measure'] = ingridient[2]
+      # print("ingridient_name {} quantity {} measure {}" .format(ingridient[0], ingridient[1], ingridient[2]))
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+  shop_list = {}
+  for dish in dishes:
+    for ingridient in cook_book[dish]:
+      new_shop_list_item = dict(ingridient)
+
+      new_shop_list_item['quantity'] *= person_count
+      if new_shop_list_item['ingridient_name'] not in shop_list:
+        shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
+      else:
+        shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
+  return shop_list
+
+def print_shop_list(shop_list):  
+  # for shop_list_item in shop_list.values():
+  #   print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'], shop_list_item['measure']))
+  for shop_list_item in shop_list.values():
+    print('{ingridient_name} {quantity} {measure}'.format(**shop_list_item))
+    
+def create_shop_list():
+  person_count = int(input('Введите количество человек: '))
+  dishes = input('Введите блюда в расчете на одного человека (через запятую): ').lower().split(', ')
+  shop_list = get_shop_list_by_dishes(dishes, person_count)
+  print_shop_list(shop_list)
+  
+# create_shop_list()
