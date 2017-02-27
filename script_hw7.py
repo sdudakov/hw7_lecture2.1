1aacf2db0bf3b5c0b9f8fddae10623cca814f9b8
Дудаков Сергей <sdudakov@corp.local>
2017-02-27 15:15:52 +0300

    cook_book не верная конструкция {'салат': {'quantity': '1', 'measure': 'шт', 'ingridient_name': 'лук'}, ...}

--- script_hw7.py
+++ script_hw7.py
@@ -16,42 +16,22 @@
 #     ]
 #   }
 
-shop_list = {}
-with open("data_hw7.txt", "r", encoding="utf-8") as f:
-  for dish in f:
-    ingridient_count = int(f.readline())
-        # print("dish = {}ingridient_count = {}" .format(dish, ingridient_count))
-    for _ in range(ingridient_count):
-      ingridient = f.readline()
-      ingridient = ingridient.strip()
-      ingridient = ingridient.split(" | ")
-      new_shop_list_item = dict({"ingridient_name": ingridient[0], "quantity": ingridient[1], "measure": ingridient[2]})
-      print(new_shop_list_item)
-      
+#Внутри функции записывайте cook_book[dish.strip()] = dict({<словарь-ингредиент>})
 
-def get_shop_list_by_dishes(dishes, person_count):
-  shop_list = {}
-  for dish in dishes:
-    for ingridient in cook_book[dish]:
-      new_shop_list_item = dict(ingridient)
 
-      new_shop_list_item['quantity'] *= person_count
-      if new_shop_list_item['ingridient_name'] not in shop_list:
-        shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
-      else:
-        shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
-  return shop_list
 
-def print_shop_list(shop_list):  
-  # for shop_list_item in shop_list.values():
-  #   print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'], shop_list_item['measure']))
-  for shop_list_item in shop_list.values():
-    print('{ingridient_name} {quantity} {measure}'.format(**shop_list_item))
-    
-def create_shop_list():
-  person_count = int(input('Введите количество человек: '))
-  dishes = input('Введите блюда в расчете на одного человека (через запятую): ').lower().split(', ')
-  shop_list = get_shop_list_by_dishes(dishes, person_count)
-  print_shop_list(shop_list)
-  
-# create_shop_list()
+def load_cook_book():
+	cook_book = {}
+	with open("data_hw7.txt", "r", encoding="utf-8") as f:
+		for dish in f:
+			ingridient_count = int(f.readline())
+			# print("dish = {}ingridient_count = {}" .format(dish, ingridient_count))
+			for _ in range(ingridient_count):
+				ingridient = f.readline()
+				ingridient = ingridient.strip()
+				ingridient = ingridient.split(" | ")			
+			cook_book[dish.strip()] = dict({"ingridient_name": ingridient[0], "quantity": ingridient[1], "measure": ingridient[2]})
+	return cook_book
+
+cook_book = load_cook_book()
+print(cook_book)
